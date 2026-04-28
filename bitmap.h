46e5b82b58f4659cf6376cb3c5f70fa6badebdec
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-04-29 00:53:52 +0200

    Move sources to src dir

--- bitmap.h
+++
@@ -1,7 +0,0 @@
-#ifndef _BITMAP_H
-#define _BITMAP_H
-
-bool IsFileBitmap(FILE *file);
-void handle_bitmap (FILE *file);
-
-#endif
--- test.asm
+++
@@ -1,7 +0,0 @@
-#macro errortest()
-  10+
-#endmacro
-#define err2 24a
-#define err3 "test"
-
- .db errortest + 1, err2, err3
