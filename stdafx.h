af7f764176e8c6cb7fe83948af030cc2d57700e7
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-04-28 22:43:19 +0200

    OpenSSL include and CMake cleanup

--- stdafx.h
+++ stdafx.h
@@ -6,9 +6,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
+// #include <strings.h>
 #include <assert.h>
-#include <unistd.h>
+// #include <unistd.h>
 #include <ctype.h>
 #include <stdarg.h>
 #ifndef NO_APPSIGN
